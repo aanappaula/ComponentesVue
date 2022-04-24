ec0c4187d6679036c5c87f7367b4052e692d2b00
aanappaula <90773998+aanappaula@users.noreply.github.com>
2022-04-24 16:23:16 -0300

    Add files via upload

---
+++ atividade componentes/componentes-vue/src/components/PrincipalComp.vue
@@ -0,0 +1,16 @@
+<script>
+import BlocoTexto from "@/components/BlocoTexto.vue"
+export default {
+    components: {BlocoTexto},
+};
+</script>
+<template>
+    <main id="content">
+        <BlocoTexto class="BlocoTexto" />
+        <BlocoTexto class="BlocoTexto"/>
+        <BlocoTexto class="BlocoTexto"/>
+        <BlocoTexto class="BlocoTexto"/>
+        <BlocoTexto class="BlocoTexto"/>
+        <BlocoTexto class="BlocoTexto"/>
+    </main>
+</template>
